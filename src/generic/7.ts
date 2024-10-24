7e2bfab9fe101e5d7d8bccc59edc8e8fd8360ce3
Kostya Nesterenko <kosnesterenko@gmail.com>
2024-10-24 22:25:47 +0300

    generics done

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,7 +10,10 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+
+type User = Record<UserRole, string>;
+
+const RoleDescription: User = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
